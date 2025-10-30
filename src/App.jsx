61eecdfb18d3819ea92d7805a5a845348b7bdd07
devadathants01 <devadathants046@gmail.com>
2025-10-30 15:23:51 +0530

    footer added

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import Demoo from "./Demoo";
 import Header from "./Header";
 import Contact from "./Contact";
+import Footer from "./assets/Footer";
 
 function App() {
   return (
     <div>
       <Header/>
       <Demoo />
+      <Footer/>
       <Contact/>
 
     </div>
